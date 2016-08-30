5e37b5ea03b8376ac96b6941ebb9d1f8482e2833
ndaughety <ndaughety@harding.edu>
2016-08-29 21:32:12 -0500

    Added LightsOut source code to repo
    This is a simple lights out game program written in C# using a GUI

---
+++ LightsOut/LightsOut/AboutForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace LightsOut
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.okButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Firebrick;
+            this.label1.Location = new System.Drawing.Point(85, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lights Out!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "By Nathan Daughety, 2016";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(221, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Turn all the lights out (black) to win the game!";
+            // 
+            // okButton
+            // 
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.Location = new System.Drawing.Point(108, 126);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 3;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(22, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(57, 60);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutForm";
+            this.Padding = new System.Windows.Forms.Padding(9);
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "About";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ LightsOut/LightsOut/MainForm.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LightsOut
+{
+    public partial class MainForm : Form
+    {
+        private const int GridOffset = 25; // Distance from upper-left side of window
+        private static int GridLength = 225; // Size in pixels of grid
+        private static int NumCells = 3; // Number of cells in grid
+        private int CellLength = GridLength / NumCells;
+
+        private bool[,] grid; // Stores on/off state of cells in grid
+        private Random rand; // Used to generate random numbers
+
+        public MainForm()
+        {
+            InitializeComponent();
+            x3ToolStripMenuItem.Checked = true;
+
+            rand = new Random(); // Initializes random number generator
+            grid = new bool[NumCells, NumCells];
+
+            // Turn entire grid on
+            for (int r = 0; r < NumCells; r++)
+            {
+                for (int c = 0; c < NumCells; c++)
+                {
+                    grid[r, c] = true;
+                }
+            }
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            for (int r = 0; r < NumCells; r++)
+                for (int c = 0; c < NumCells; c++)
+                {
+                    // Get proper pen and brush for on/off
+                    // grid section
+                    Brush brush;
+                    Pen pen;
+
+                    if (grid[r, c])
+                    {
+                        pen = Pens.Black;
+                        brush = Brushes.White; // On
+                    }
+                    else
+                    {
+                        pen = Pens.White;
+                        brush = Brushes.Black; // Off
+                    }
+
+                    CellLength = GridLength / NumCells;
+
+                    // Determine (x,y) coord of row and col to draw rectangle
+                    int x = c * CellLength + GridOffset;
+                    int y = r * CellLength + GridOffset;
+
+                    // Draw outline and inner rectangle
+                    g.DrawRectangle(pen, x, y, CellLength, CellLength);
+                    g.FillRectangle(brush, x + 1, y + 1, CellLength - 1, CellLength - 1);
+                }
+        }
+
+        private void InitializeBoard()
+        {
+            grid = new bool[NumCells, NumCells];
+
+            // redraw board
+            this.Invalidate();
+
+            // reset checked menu option
+            if (NumCells == 3)
+            {
+                x3ToolStripMenuItem.Checked = true;
+                x4ToolStripMenuItem.Checked = false;
+                x5ToolStripMenuItem.Checked = false;
+            }
+            else if (NumCells == 4)
+            {
+                x3ToolStripMenuItem.Checked = false;
+                x4ToolStripMenuItem.Checked = true;
+                x5ToolStripMenuItem.Checked = false;
+            }
+            else
+            {
+                x3ToolStripMenuItem.Checked = false;
+                x4ToolStripMenuItem.Checked = false;
+                x5ToolStripMenuItem.Checked = true;
+            }
+
+            // Turn entire grid on
+            for (int r = 0; r < NumCells; r++)
+            {
+                for (int c = 0; c < NumCells; c++)
+                {
+                    grid[r, c] = true;
+                }
+            }
+        }
+
+        private void x3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NumCells = 3;
+            InitializeBoard();
+        }
+
+        private void x4ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NumCells = 4;
+            InitializeBoard();
+        }
+
+        private void x5ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NumCells = 5;
+            InitializeBoard();
+        }
+
+        private bool PlayerWon()
+        {
+            bool result = false;
+
+            for (int r = 0; r < NumCells; r++)
+                for (int c = 0; c < NumCells; c++)
+                {
+                    if (grid[r, c] == false)
+                        result = true;
+                    else
+                        return false;
+                }
+            return result;
+        }
+
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Make sure click was inside the grid
+            if (e.X < GridOffset || e.X > CellLength * NumCells + GridOffset ||
+            e.Y < GridOffset || e.Y > CellLength * NumCells + GridOffset)
+                return;
+
+            // Find row, col of mouse press
+            int r = (e.Y - GridOffset) / CellLength;
+            int c = (e.X - GridOffset) / CellLength;
+
+            // Invert selected box and all surrounding boxes
+            for (int i = r - 1; i <= r + 1; i++)
+                for (int j = c - 1; j <= c + 1; j++)
+                    if (i >= 0 && i < NumCells && j >= 0 && j < NumCells)
+                        grid[i, j] = !grid[i, j];
+
+            // Redraw grid
+            this.Invalidate();
+
+            // Check to see if puzzle has been solved
+            if (PlayerWon())
+            {
+                // Display winner dialog box
+                MessageBox.Show(this, "Congratulations! You've won!", "Lights Out!",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            // Fill grid with either white or black
+            for (int r = 0; r < NumCells; r++)
+                for (int c = 0; c < NumCells; c++)
+                    grid[r, c] = rand.Next(2) == 1;
+
+            // Redraw grid
+            this.Invalidate();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            newGameButton_Click(sender, e);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutForm aboutBox = new AboutForm();
+            aboutBox.ShowDialog(this);
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            Control control = (Control)sender;
+
+            /***********************************************************************************************
+             * Code example from:
+             * https://msdn.microsoft.com/en-us/library/system.windows.forms.control.resize(v=vs.110).aspx
+            ***********************************************************************************************/
+            // Make Form remain a square
+            if (control.Size.Height != control.Size.Width)
+            {
+                control.Size = new Size(control.Size.Width, control.Size.Width);
+            }
+
+            GridLength = control.Size.Width - 125;
+            this.Invalidate();
+        }
+    }
+}
